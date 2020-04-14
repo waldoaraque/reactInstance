1ba308a02ee250e2963bda4bb5608392f8966310
waldev <waldoaraque10@gmail.com>
2020-04-14 15:41:47 +0200

    creating a component with ES6 sintax

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+//Fragment puede exportar varios elementos... Sin necesidad de meterlo todo en un <div>
+class Header extends Component {
+    render() {
+        return(
+            <h1>Hello World</h1>
+        )
+    }
+}
+
+export default Header;
